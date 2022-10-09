18de770784227892b189b551016bae784ad92f4e
JisungPark <jisung9870@gmail.com>
2022-10-09 23:20:58 +0900

    Add namenode and datanode

---
+++ hadoop/namenode/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NAME_DIR=$1
+echo $NAME_DIR
+
+if [ "$(ls -A $NAME_DIR)" ]; then
+    echo "NameNode is a already formatted."
+else
+    echo "Format NameNode"
+    $HADOOP_PREFIX/bin/hdfs --config $HADOOP_CONF_DIR namenode -format
+fi
+
+$HADOOP_PREFIX/bin/hdfs --config $HADOOP_CONF_DIR namenode
